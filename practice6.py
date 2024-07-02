ed7be3b5beab6bc8c7cd24b2a76d4eb188ab7b89
bradzara <bradzara93@gmail.com>
2024-07-02 09:01:01 -0700

    python practice

---
+++ practice6.py
@@ -0,0 +1,69 @@
+# Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+# numbers = [5, 4, 8, 12, 3]
+# numbers[0] = numbers[0] * 10
+# print(numbers)
+
+# Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+# countries = ["America", "Italy", "Germany"]
+# countries.append("Poland")
+# print(countries)
+
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+# person = {"first name": "Bradley", "last name": "Zara", "email": "brad@email.com"}
+# print(person["first name"])
+# print(person["last name"])
+# print(person["email"])
+
+# Make a hash to store 3 different states and their capitals. Then add a new state and capital and print the hash to see the result.
+
+# states = {"New Jersey": "Trenton", "Pennsylvania": "Harrisburg", "California": "Sacramento"}
+# states["Florida"] = "Tallahassee"
+# print(states)
+
+# Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+
+# def make_upcase(str):
+#   return str.upper()
+
+# print(make_upcase("hello"))
+
+# Write a method that takes in a number and returns the number times itself. Then run the method and print the result.
+
+# def squared(number):
+#   return number * number
+# print(squared(3))
+
+
+# Write a method that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the method and print the result.
+
+# def average(num1, num2, num3):
+#   return (num1 + num2 + num3) / 3
+
+# print(average(2, 2, 2))
+
+# Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
+
+# def equate(number):
+#   return (number * 10) + 30
+
+# print (equate(10))
+
+# Write a Book class with attributes for title, author, and year.
+
+class Book:
+  def __init__(self, title, author, year):
+    self.title = title
+    self.author = author
+    self.year = year
+
+  def __str__(self):
+    return f"Book(title='{self.title}', author='{self.author}', author='{self.year}')"
+
+  def __repr__(self):
+    return self.__str__()
+
+book = Book("example", "Guy", 2019)
+print(book)
