2ae1c6764ae94a95578d9ff0d8ea94dcfb5a44cf
bradzara <bradzara93@gmail.com>
2024-07-30 12:05:26 -0700

    Return f string

--- hackerrank.py
+++ hackerrank.py
@@ -16,3 +16,14 @@ def split_and_join(string):
   string = "-".join(string)
   return string
 print(split_and_join(string))
+
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+# Hello firstname lastname! You just delved into python.
+
+first = "John"
+last = "Smith"
+
+def print_full_name(first, last):
+  return f"Hello {first} {last}! You just delved into python."
+
+print(print_full_name(first, last))
