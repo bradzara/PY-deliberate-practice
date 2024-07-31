3d63436bdaa23d0c1348df3f906af14b6bc4366d
bradzara <bradzara93@gmail.com>
2024-07-31 12:26:26 -0700

    Nested loops: largest sum of two different numbers in array

--- practice16.py
+++ practice16.py
@@ -13,3 +13,20 @@ while index1 < len(numbers):
   index1 += 1
 print(new_numbers)
 
+# Use a nested loop to find the largest sum of any two different numbers within an array.
+# For example, [1, 8, 3, 10] becomes 18.
+
+numbers = [1, 8, 3, 10]
+largest = numbers[0] + numbers[1]
+index1 = 0
+while index1 < len(numbers):
+  number = numbers[index1]            
+  index2 = 0
+  while index2 < len(numbers):
+    if numbers[index2] != number:
+      sum = number + numbers[index2]   
+      if sum > largest:
+        largest = sum
+    index2 += 1
+  index1 += 1
+print(largest)
