3636628bc9dbb576def4721e5f927e44fa5f224d
bradzara <bradzara93@gmail.com>
2024-07-31 12:38:37 -0700

    nested loops: return first two numbers == 10

--- practice16.py
+++ practice16.py
@@ -30,3 +30,20 @@ while index1 < len(numbers):
     index2 += 1
   index1 += 1
 print(largest)
+
+# Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false. For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+
+numbers = [2, 5, 3, 7, 0, 1, 11]
+ten = False
+index1 = 0
+while index1 < len(numbers):
+  number = numbers[index1]
+  index2 = 0
+  while index2 < len(numbers):
+    if index1 != index2:
+      other_number = numbers[index2]
+      if number + other_number == 10 and ten == False:
+        ten = [number, other_number]
+    index2 += 1
+  index1 += 1
+print(ten)
