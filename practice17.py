3eb5dd21c5dc01ff3b681008fd6ca534aa445a00
bradzara <bradzara93@gmail.com>
2024-08-02 12:01:01 -0700

    convert arrays into hash

--- practice17.py
+++ practice17.py
@@ -14,3 +14,18 @@ while index1 < len(strings):
   index1 += 1
 
 print(combined_string)
+
+# Convert an array of arrays into a hash.
+# For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+
+pairs = [[1, 3], [8, 9], [2, 16]]
+pairs_hash = {}
+index = 0
+while index < len(pairs):
+  key = pairs[index][0]
+  value = pairs[index][1]
+  pairs_hash[key] = value
+  index += 1
+print(pairs_hash)
+
+
