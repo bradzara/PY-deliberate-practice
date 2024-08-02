189868fffbf72a2650cfe2b479e06cf8f1b4bd21
bradzara <bradzara93@gmail.com>
2024-08-02 08:50:44 -0700

    Check character cases exercise

--- hackerrank.py
+++ hackerrank.py
@@ -67,3 +67,19 @@ unique_scores.sort(reverse=True)
 
 runner_up_score = unique_scores[1]
 print(runner_up_score)
+
+
+# In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+# In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+# In the third line, print True if  has any digits. Otherwise, print False.
+# In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+# In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+
+s = "qA2"
+
+print(any(c.isalnum() for c in s))
+print(any(c.isalpha() for c in s))
+print(any(c.isdigit() for c in s))
+print(any(c.islower() for c in s))
+print(any(c.isupper() for c in s))
+
