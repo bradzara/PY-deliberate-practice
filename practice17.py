7060ce1f082ff6c9c4436334ba1e05ec5c23dcf4
bradzara <bradzara93@gmail.com>
2024-08-02 12:10:11 -0700

    Convert array of hashes into hash

--- practice17.py
+++ practice17.py
@@ -29,3 +29,12 @@ while index < len(pairs):
 print(pairs_hash)
 
 
+# Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash. For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+array = [{"id": 1, "color": "blue", "price": 32}, {"id": 2, "color": "red", "price": 12}]
+hash = {}
+index = 0
+while index < len(array):
+  hash[array[index]["id"]] = array[index]
+  index += 1
+print(hash)
