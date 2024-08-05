acc69b442c152a56c80da243b871de1399a36d8c
bradzara <bradzara93@gmail.com>
2024-08-04 18:55:52 -0700

    String concatenation

---
+++ practice18.py
@@ -0,0 +1,5 @@
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first = "John"
+last = "Smith"
+print(first + " " + last)
