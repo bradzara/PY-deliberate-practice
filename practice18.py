c3af99df47a7b7fa0a627ba34ed75fce49287681
bradzara <bradzara93@gmail.com>
2024-08-04 19:04:16 -0700

    create new array after computation

--- practice18.py
+++ practice18.py
@@ -12,6 +12,21 @@ print(f"{first} {last}")
 
 # Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
-word = input("Input a word: ")
-if word == "marco":
-  print("polo")
+# word = input("Input a word: ")
+# if word == "marco":
+#   print("polo")
+
+
+# Start with an array of numbers and create a new array with each number times 3.
+# For example, [1, 2, 3] becomes [3, 6, 9].
+
+numbers = [1, 2, 3]
+new_numbers = [number * 3 for number in numbers]
+print(new_numbers)
+
+# Start with an array of numbers and create a new array with each number plus 7.
+# For example, [1, 2, 3] becomes [8, 9, 10].
+
+numbers = [1, 2, 3]
+new_numbers = [number + 7 for number in numbers]
+print(new_numbers)
