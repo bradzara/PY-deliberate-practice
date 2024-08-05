355e9bcbf2ad19469022c594332146d3a5ed54c0
bradzara <bradzara93@gmail.com>
2024-08-04 18:57:02 -0700

    string interpolation

--- practice18.py
+++ practice18.py
@@ -3,3 +3,9 @@
 first = "John"
 last = "Smith"
 print(first + " " + last)
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first = "John"
+last = "Smith"
+print(f"{first} {last}")
