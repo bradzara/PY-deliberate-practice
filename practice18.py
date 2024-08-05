aea9dfc29a618e04d7e73425d33f591b9fb4a905
bradzara <bradzara93@gmail.com>
2024-08-04 19:30:15 -0700

    Find total after discount

--- practice18.py
+++ practice18.py
@@ -94,3 +94,26 @@ for number in numbers:
     smallest = number
 print(smallest)
 
+# Write a program that stores a person's order value and membership level (regular or premium). Then calculate a discount amount based on the following conditions:
+
+# If the total order value is less than $50, there is no discount.
+# If the total order value is between $50 and $100, the discount is 5% for regular customers and 10% for premium customers.
+# If the total order value is greater than $100, the discount is 10% for regular customers and 15% for premium customers.
+
+total = 108.73
+membership = "regular"
+
+if total < 50:
+  total = total
+elif total >= 50 and total <= 100:
+  if membership == "regular":
+    total *= .95
+  else:
+    total *= .90
+else:
+  if membership == "regular":
+    total *= .90
+  else:
+    total *= .85
+total = round(total, 2)
+print(f"${total}")
